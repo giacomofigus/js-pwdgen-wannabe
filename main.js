1b0f5fad30dc7b24cee5a5f4a432f8e12d9eff5c
MyUsername <figusgiacomo04@gmail.com>
2023-12-21 15:39:02 +0100

    document.writeln

--- main.js
+++ main.js
@@ -2,3 +2,4 @@ let nomeUtente = prompt('Inserisci il tuo nome');
 let cognomeUtente = prompt ('Inserisci il tuo cognome');
 let coloreUtente = prompt ('Inserici il tuo colore preferito');
 
+document.writeln(nomeUtente + cognomeUtente + coloreUtente + 2 + 3);
